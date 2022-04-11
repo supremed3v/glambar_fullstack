23b9ac6a1f78220c992263338da43cc019e6a9fd
Shaftain Zohra <shaftainzohra.2000@gmail.com>
2022-04-11 20:16:08 +0500

    home screen icon added

--- frontend/src/screens/Home/Home.js
+++ frontend/src/screens/Home/Home.js
@@ -11,13 +11,25 @@ import {
 import React, { useState } from "react";
 import CustomInput from "../../components/CustomInput";
 import Ionicons from "@expo/vector-icons/Ionicons";
+import Feather from "@expo/vector-icons/Feather"; 
+import Entypo from "@expo/vector-icons/Entypo"; 
+import AntDesign from "@expo/vector-icons/AntDesign"; 
 import myData from "../../DummyData/SalonHomeData";
 
 const Home = ({ navigation }) => {
   const [search, setSearch] = useState("");
 
   return (
+    
     <ScrollView showsVerticalScrollIndicator={false}>
+      <View style={styles.topnav}>
+      <AntDesign name="menuunfold" size={24} color="black" />
+      <Text> G 14/4, Islamabad</Text>
+      <View style={styles.topnav2}>
+      <Entypo name="bell" size={24} color="black" />
+      <Feather name="shopping-cart" size={24} color="black" />
+      </View>
+              </View> 
       <SafeAreaView>
         <FlatList
           data={myData}
@@ -55,7 +67,7 @@ const Home = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     alignItems: "center",
-    marginTop: 50,
+    marginTop: 10,
     paddingHorizontal: 20,
   },
 
@@ -86,6 +98,20 @@ const styles = StyleSheet.create({
   textRating: {
     paddingLeft: 90,
   },
+  topnav:{
+    flexDirection: "row",
+    marginTop: 10,
+    padding: 10,
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  topnav2:{
+    flexDirection: "row",
+    marginTop: 10,
+    padding: 10,
+    justifyContent: "space-between",
+    alignItems: "center"
+  }
 });
 
 export default Home;
